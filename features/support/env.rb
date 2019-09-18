5fd3580e68fadf01bc83d51163524d7590cd11b9
I Love My QA <tech@ilovemyqa.com>
2019-09-18 20:22:24 +0300

    update env.rb 4

--- features/support/env.rb
+++ features/support/env.rb
@@ -7,7 +7,7 @@ require 'report_builder'
 ENV['BS_USERNAME'] = 'marcborowy1'
 ENV['BS_AUTHKEY'] = 'qxqmjPtLVVfyRvuSBmR4'
 
-ENV['BS_PLATFORM'] = 'WINDOWS10'
+#ENV['BS_PLATFORM'] = 'WINDOWS10'
 
 url = "http://#{ENV['BS_USERNAME']}:#{ENV['BS_AUTHKEY']}@hub-cloud.browserstack.com/wd/hub"
 
@@ -20,8 +20,8 @@ if ENV['BS_AUTOMATE_OS']
   capabilities['os'] = ENV['BS_AUTOMATE_OS']
   capabilities['os_version'] = ENV['BS_AUTOMATE_OS_VERSION']
 else
-  # capabilities['platform'] = ENV['SELENIUM_PLATFORM'] || 'ANY'
-  capabilities['platform'] = ENV['BS_PLATFORM']
+  capabilities['platform'] = ENV['SELENIUM_PLATFORM'] || 'ANY'
+  #capabilities['platform'] = ENV['BS_PLATFORM']
 end
 
 capabilities['browser'] = ENV['SELENIUM_BROWSER'] || 'chrome'
@@ -35,25 +35,25 @@ capabilities['browserstack.console'] = 'errors'
 capabilities['browserstack.networkLogs'] = 'true'
 capabilities['browserstack.appiumLogs'] = 'true'
 
-if ENV['BS_PLATFORM'] == 'MAC_MOJAVE_CHROME'
-  capabilities['browser'] = 'Chrome'
-  capabilities['browser_version'] = '76.0'
-  capabilities['os'] = 'OS X'
-  capabilities['os_version'] = 'Mojave'
-  capabilities['resolution'] = '1920x1080'
-elsif ENV['BS_PLATFORM'] == 'MAC_MOJAVE_SAFARI'
-  capabilities['browser'] = 'Safari'
-  capabilities['browser_version'] = '12.1'
-  capabilities['os'] = 'OS X'
-  capabilities['os_version'] = 'Mojave'
-  capabilities['resolution'] = '1920x1080'
-elsif ENV['BS_PLATFORM'] == 'WINDOWS10'
-  capabilities['browser'] = 'Chrome'
-  capabilities['browser_version'] = '76.0'
-  capabilities['os'] = 'Windows'
-  capabilities['os_version'] = '10'
-  capabilities['resolution'] = '1920x1080'
-elsif ENV['BS_PLATFORM'] == 'LOCAL'
+#if ENV['BS_PLATFORM'] == 'MAC_MOJAVE_CHROME'
+#  capabilities['browser'] = 'Chrome'
+#  capabilities['browser_version'] = '76.0'
+#  capabilities['os'] = 'OS X'
+#  capabilities['os_version'] = 'Mojave'
+#  capabilities['resolution'] = '1920x1080'
+#elsif ENV['BS_PLATFORM'] == 'MAC_MOJAVE_SAFARI'
+#  capabilities['browser'] = 'Safari'
+#  capabilities['browser_version'] = '12.1'
+#  capabilities['os'] = 'OS X'
+#  capabilities['os_version'] = 'Mojave'
+#  capabilities['resolution'] = '1920x1080'
+#elsif ENV['BS_PLATFORM'] == 'WINDOWS10'
+#  capabilities['browser'] = 'Chrome'
+#  capabilities['browser_version'] = '76.0'
+#  capabilities['os'] = 'Windows'
+#  capabilities['os_version'] = '10'
+#  capabilities['resolution'] = '1920x1080'
+#elsif ENV['BS_PLATFORM'] == 'LOCAL'
   # Browser Chrome for macOS and Windows
   to_chrome_mac_path = 'drivers/macOS/chromedriver' # Chrome driver for macOS
   to_chrome_win_path = 'drivers/Windows/chromedriver.exe' # Chrome driver for Windows
@@ -61,13 +61,19 @@ elsif ENV['BS_PLATFORM'] == 'LOCAL'
   # Browser Firefox for macOS and Windows
   to_firefox_mac_path = 'drivers/Windows/geckodriver.exe' # Firefox driver for macOS
   to_firefox_win_path = 'drivers/Windows/geckodriver.exe' # Firefox driver for Windows
-else
-  capabilities['browser'] = 'Chrome'
-  capabilities['browser_version'] = '76.0'
-  capabilities['os'] = 'Windows'
-  capabilities['os_version'] = '7'
-  capabilities['resolution'] = '1920x1080'
-end
+#else
+#  capabilities['browser'] = 'Chrome'
+#  capabilities['browser_version'] = '76.0'
+#  capabilities['os'] = 'Windows'
+#  capabilities['os_version'] = '7'
+#  capabilities['resolution'] = '1920x1080'
+#end
+
+capabilities['browser'] = 'Chrome'
+capabilities['browser_version'] = '76.0'
+capabilities['os'] = 'Windows'
+capabilities['os_version'] = '10'
+capabilities['resolution'] = '1920x1080'
 
 #if ENV['BS_PLATFORM'] == 'LOCAL'
 #  browser = Selenium::WebDriver.for :chrome, driver_path: to_chrome_mac_path
