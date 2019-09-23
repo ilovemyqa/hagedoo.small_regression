95dba1b16d0d7b3ae833aa4a4b3df3cc82249d83
I Love My QA <tech@ilovemyqa.com>
2019-09-23 18:04:14 +0300

    update how_it_works

--- features/step_definitions/how_it_works.rb
+++ features/step_definitions/how_it_works.rb
@@ -1,17 +1,20 @@
 Given(/^I open browser for testing page\.$/) do
   @browser.manage.window.maximize
-  @wait = Selenium::WebDriver::Wait.new(timeout: 20)
+  @wait = Selenium::WebDriver::Wait.new(timeout: 60)
   # url = 'https://hagedoo.de'
   @url_project = 'https://staging.hagedoo.de/'
   @browser.navigate.to @url_project
 end
 
 When(/^I go to How it work page\.$/) do
-  @browser.find_element(xpath: "//li[@class='header-item header-how-it-works ']").click
-  @wait.until { @browser.find_element(xpath: "//div[@class='header-container']").displayed? }
+  # @browser.find_element(xpath: "//li[@class='header-item header-how-it-works ']").click
+  # @wait.until { @browser.find_element(xpath: "//div[@class='header-container']").displayed? }
+
+  @browser.navigate.to @url_project + "wie-es-funktioniert"
 end
 
 And(/^I find element title\.$/) do
+  @wait.until { @browser.find_element(xpath: "//div[@class='header-container']").displayed? }
   @browser.find_element(xpath: "//div[@class='header-container']")
 end
 
